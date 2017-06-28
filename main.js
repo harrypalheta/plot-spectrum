db2dbac6b4fd0256d1984bf6b7e10b0b56e8bac1
Harryson Palheta <harryson,palheta@gmail.com>
2017-06-28 14:51:09 -0400

    Implantação do chartjs.

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+var Chart = require("chart.js");
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
